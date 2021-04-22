b9c5b410e9804efb177c355967e0f60904175eef
cliche <745356675@qq.com>
2021-04-22 21:31:37 +0800

    -

--- 外排序-竞赛树/main.cpp
+++ 外排序-竞赛树/main.cpp
@@ -2,6 +2,13 @@
 #include "minLoserTree.cpp"
 #include "externalSorter.cpp"
 
+/*
+缓冲区大 - 归并路数大 --> 归并次数小一点 : 每个顺串分配到的空间小，可能需要多次访存，但是归并次数小
+缓冲区大 - 归并路数小 --> 归并次数多一点，但是多不了太多 : 每个顺串分配到的空间大，访存次数可能要小一点
+缓冲区小 - 归并路数大 --> 归并次数小 : 每个顺串分配到的空间更小，访存次数一定比较多
+缓冲区小 - 归并路数小 --> 归并次数多 : 每个顺串分配到的空间相对大一点，访存次数相对小一点
+*/
+
 int main() {
     string rootDirectory = "/Users/abc_mac/Documents/Code/数据结构课设/外排序-竞赛树/data/";
     string inFilename;
