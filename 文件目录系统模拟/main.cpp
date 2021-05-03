9ff4b1bc641f6dad31c3e5e93845b7be64417b27
cliche <745356675@qq.com>
2021-05-03 21:30:46 +0800

    -

---
+++ 文件目录系统模拟/main.cpp
@@ -0,0 +1,12 @@
+#include "catalogTree.h"
+
+int main() {
+    string rootName;
+    cout << "请输入您要设置的根目录名称: ";
+    cin >> rootName;
+    catalogTree fileTree(rootName);
+
+    while (true) {
+        
+    }
+}
