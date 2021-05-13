ed0bd782e9a8ea76c2c39b70c1957b68fedc1dcb
cliche <745356675@qq.com>
2021-05-13 20:06:12 +0800

    -

--- 网络放大器设置/linkedWDigraph.h
+++ 网络放大器设置/linkedWDigraph.h
@@ -18,7 +18,6 @@ struct vertexNode {
     int number;                 // 编号
     int inDegree;               // 节点的入度, 用于拓扑序
     int pressure;               // 压力值
-    vector<wEdge> preEdges;     // 终点为this的边集
     vector<wEdge> edges;        // 起点为this的边集
     vertexNode(int theNumber = 0, int p = 0): number(theNumber), pressure(p), inDegree(0) {
         edges.clear();
@@ -31,17 +30,18 @@ private:
     int numberOfEdges;          // 边的个数
     int Pmin, Pmax;             // 压力最小值 & 加压后的压力
     int numberOfBoosters;       // 加压器个数
-    vertexNode *vertexOf;       // 节点集
-    int *sequence;              // 节点访问顺序
-    bool *boosterHere;          // 是否放石油放大器
+    vector<vertexNode> vertexOf;       // 节点集
+    vector<int> sequence;              // 节点访问顺序
+    vector<bool> boosterHere;          // 是否放石油放大器
 public:
     DAG() {
         cin >> numberOfVertex >> numberOfEdges >> Pmax;
+        numberOfBoosters = numberOfVertex;
         Pmin = 0;
-        vertexOf = new vertexNode[numberOfVertex + 1];
-        sequence = new int[numberOfVertex];
-        boosterHere = new bool[numberOfVertex + 1];
         // 初始化元素
+        vertexOf.resize(numberOfVertex + 1);
+        sequence.resize(numberOfVertex);
+        boosterHere.resize(numberOfVertex + 1);
         for (int i = 1; i <= numberOfVertex; ++i) {
             vertexOf[i].number = i;
             boosterHere[i] = false;
@@ -52,7 +52,6 @@ public:
             cin >> u >> v >> w;
             ++vertexOf[v].inDegree;
             vertexOf[u].edges.push_back(wEdge(u, v, w));
-            vertexOf[v].preEdges.push_back(wEdge(u, v, w));
         }
         // 求出节点访问顺序
         topoSort();
@@ -81,7 +80,7 @@ public:
     void placeBooster(int level, int number_of_boosters) {
         // 标识是否需要放放大器
         bool tag = false;
-
+        // 判断是否结束
         if (level >= numberOfVertex - 1) {
             // 到达决策树底层, 可以返回了
             if (number_of_boosters < numberOfBoosters) {
@@ -104,33 +103,29 @@ public:
         for (int i = 0; i < level; ++i)
             for (vector<wEdge>::iterator e = vertexOf[sequence[i]].edges.begin(); e != vertexOf[sequence[i]].edges.end(); ++e)
                 if (e->to == theVertexNumber)
+                    // 需要求出到达该点的最大压力, 但是同时又要考虑是否能到达全部边, 这里需要考虑吗?
                     vertexOf[theVertexNumber].pressure = max(vertexOf[theVertexNumber].pressure, vertexOf[sequence[i]].pressure - e->weight);
 
-        int k = 0;
-        for (; k < 2; ++k) {
-            if (vertexOf[theVertexNumber].pressure == Pmax) {
-                boosterHere[level] = false;
-                placeBooster(level + 1, number_of_boosters);
-                break;
-            }
-            if (vertexOf[theVertexNumber].pressure <= Pmin) {
-                boosterHere[level] = true;
-                ++number_of_boosters;
-                vertexOf[theVertexNumber].pressure = Pmax;
-                if (number_of_boosters >= numberOfBoosters)
-                    return;
-                placeBooster(level + 1, number_of_boosters);
-                break;
-            }
-            if (k == 0) {
-                for (vector<wEdge>::iterator e = vertexOf[theVertexNumber].edges.begin(); e != vertexOf[theVertexNumber].edges.end(); ++e)
-                    if (vertexOf[theVertexNumber].pressure - e->weight < Pmin) {
-                        tag = true;
-                        break;
-                    }
-            }
+        if (vertexOf[theVertexNumber].pressure <= Pmin) {
+            // 从前面的点无法到达该点/到达的压力 <= 0, 必须放一个放大器
+            boosterHere[level] = true;
+            ++number_of_boosters;
+            vertexOf[theVertexNumber].pressure = Pmax;
+            if (number_of_boosters >= numberOfBoosters)
+                // 此时如果已经 >= 最优结果了, 不需要再向下了, 剪枝
+                return;
+            placeBooster(level + 1, number_of_boosters);
         }
-        if (tag == true || k == 1) {
+        else {
+            for (vector<wEdge>::iterator e = vertexOf[theVertexNumber].edges.begin(); e != vertexOf[theVertexNumber].edges.end(); ++e)
+                if (vertexOf[theVertexNumber].pressure - e->weight < Pmin) {
+                    // 此处tag标识是否有边无法通过, 如果有说明需要加放大器
+                    tag = true;
+                    break;
+                }
+        }
+        // 如果有边无法通过, 加放大器
+        if (tag == true) {
             boosterHere[level] = true;
             ++number_of_boosters;
             vertexOf[theVertexNumber].pressure = Pmax;
@@ -138,17 +133,15 @@ public:
                 return;
             placeBooster(level + 1, number_of_boosters);
         }
+        // 所有边都可以通过, 不需要加放大器
         else {
             boosterHere[level] = false;
             placeBooster(level + 1, number_of_boosters);
         }
     }
-
-    void solution1() {
-        for (int i = 0; i < numberOfVertex; ++i) {
-            int s = sequence[i];
-
-        }
+    // 输出结果
+    void output() const {
+        cout << numberOfBoosters << endl;
     }
 };
 
---
+++ 网络放大器设置/solution.cpp
@@ -0,0 +1,15 @@
+#include "linkedWDigraph.h"
+
+void solution1() {
+    DAG graph;
+    graph.placeBooster(0, 0);
+    graph.output();
+}
+
+int main() {
+    freopen("/Users/abc_mac/Documents/Code/数据结构课设/网络放大器设置/data/input/input35.in", "r", stdin);
+    freopen("/Users/abc_mac/Documents/Code/数据结构课设/网络放大器设置/data/output/35.out", "w", stdout);
+    solution1();
+
+    return 0;
+}
