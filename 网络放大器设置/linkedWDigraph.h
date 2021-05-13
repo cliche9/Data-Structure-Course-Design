853e74913883a4cda2f2796f52ad6ea0eac6b976
cliche <745356675@qq.com>
2021-05-13 19:27:35 +0800

    -

--- 网络放大器设置/linkedWDigraph.h
+++ 网络放大器设置/linkedWDigraph.h
@@ -18,11 +18,9 @@ struct vertexNode {
     int number;                 // 编号
     int inDegree;               // 节点的入度, 用于拓扑序
     int pressure;               // 压力值
-    int degradeToEnd;           // 当前节点到终点的衰减量
-    bool boosterHere;           // 是否放石油放大器
     vector<wEdge> preEdges;     // 终点为this的边集
     vector<wEdge> edges;        // 起点为this的边集
-    vertexNode(int theNumber = 0, int p = 0): number(theNumber), pressure(p), inDegree(0), degradeToEnd(0), boosterHere(false) {
+    vertexNode(int theNumber = 0, int p = 0): number(theNumber), pressure(p), inDegree(0) {
         edges.clear();
     }
 };
@@ -32,64 +30,118 @@ private:
     int numberOfVertex;         // 节点个数
     int numberOfEdges;          // 边的个数
     int Pmin, Pmax;             // 压力最小值 & 加压后的压力
-    int numberOfboosters;          // 加压器个数
-    vector<vertexNode> vertexes;   // 节点集
-    vector<int> sequence;          // 节点访问顺序
+    int numberOfBoosters;       // 加压器个数
+    vertexNode *vertexOf;       // 节点集
+    int *sequence;              // 节点访问顺序
+    bool *boosterHere;          // 是否放石油放大器
 public:
     DAG() {
         cin >> numberOfVertex >> numberOfEdges >> Pmax;
         Pmin = 0;
-        vertexes.clear(); 
+        vertexOf = new vertexNode[numberOfVertex + 1];
+        sequence = new int[numberOfVertex];
+        boosterHere = new bool[numberOfVertex + 1];
         // 初始化元素
-        for (int i = 1; i <= numberOfVertex; ++i)
-            vertexes.push_back(vertexNode(i, Pmax));
+        for (int i = 1; i <= numberOfVertex; ++i) {
+            vertexOf[i].number = i;
+            boosterHere[i] = false;
+        }
         // 读入m条边
         for (int i = 0; i < numberOfEdges; ++i) {
             int u, v, w;
             cin >> u >> v >> w;
-            ++vertexes[v].inDegree;
-            vertexes[u].edges.push_back(wEdge(u, v, w));
-            vertexes[v].preEdges.push_back(wEdge(u, v, w));
+            ++vertexOf[v].inDegree;
+            vertexOf[u].edges.push_back(wEdge(u, v, w));
+            vertexOf[v].preEdges.push_back(wEdge(u, v, w));
         }
+        // 求出节点访问顺序
+        topoSort();
     }
     // 拓扑排序, 找出节点的访问顺序, 每个节点都要先访问完前面的节点再访问
     void topoSort() {
         int inDegreeOf[numberOfVertex + 1];
+        int now = 0;
         queue<int> q;
         for (int i = 1; i <= numberOfVertex; ++i) {
-            inDegreeOf[i] = vertexes[i].inDegree;
-            if (!vertexes[i].inDegree)
+            inDegreeOf[i] = vertexOf[i].inDegree;
+            if (!vertexOf[i].inDegree)
                 q.push(i);
         }
 
         while (!q.empty()) {
             int v = q.front();
             q.pop();
-            sequence.push_back(v);
-            for (vector<wEdge>::iterator e = vertexes[v].edges.begin(); e != vertexes[v].edges.end(); ++e)
+            sequence[now++] = v;
+            for (vector<wEdge>::iterator e = vertexOf[v].edges.begin(); e != vertexOf[v].edges.end(); ++e)
                 if ((--inDegreeOf[e->to]) == 0)
                     q.push(e->to);
         }
     }
+    // 回溯放置放大器
+    void placeBooster(int level, int number_of_boosters) {
+        // 标识是否需要放放大器
+        bool tag = false;
 
-    void placeBooster(int source) {        
-        // 更新当前点的pressure
+        if (level >= numberOfVertex - 1) {
+            // 到达决策树底层, 可以返回了
+            if (number_of_boosters < numberOfBoosters) {
+                // 找到更优解, 更新结果
+                numberOfBoosters = number_of_boosters;
+            }
+            return;
+        }
+        // 找到该层决策的节点
+        int theVertexNumber = sequence[level];
+        vertexOf[theVertexNumber].pressure = 0;
+        // 特殊处理源点
+        if (level == 0) {
+            vertexOf[theVertexNumber].pressure = Pmax;
+            boosterHere[level] = false;
+            placeBooster(1, 0);
+            return;
+        }
+        // 求theVertexNumber处的油压
+        for (int i = 0; i < level; ++i)
+            for (vector<wEdge>::iterator e = vertexOf[sequence[i]].edges.begin(); e != vertexOf[sequence[i]].edges.end(); ++e)
+                if (e->to == theVertexNumber)
+                    vertexOf[theVertexNumber].pressure = max(vertexOf[theVertexNumber].pressure, vertexOf[sequence[i]].pressure - e->weight);
 
-        int maxCost = 0;
-        // 回溯, 从当前点出发, 找可达点, 进行同样的操作, 记录下到达所有可达点所需要的最小压力 - 即最大消耗
-        for (vector<wEdge>::iterator e = vertexes[source].edges.begin(); e != vertexes[source].edges.end(); ++e) {
-            placeBooster(e->to);
-            maxCost = max(maxCost, e->weight + vertexes[e->to].degradeToEnd);
+        int k = 0;
+        for (; k < 2; ++k) {
+            if (vertexOf[theVertexNumber].pressure == Pmax) {
+                boosterHere[level] = false;
+                placeBooster(level + 1, number_of_boosters);
+                break;
+            }
+            if (vertexOf[theVertexNumber].pressure <= Pmin) {
+                boosterHere[level] = true;
+                ++number_of_boosters;
+                vertexOf[theVertexNumber].pressure = Pmax;
+                if (number_of_boosters >= numberOfBoosters)
+                    return;
+                placeBooster(level + 1, number_of_boosters);
+                break;
+            }
+            if (k == 0) {
+                for (vector<wEdge>::iterator e = vertexOf[theVertexNumber].edges.begin(); e != vertexOf[theVertexNumber].edges.end(); ++e)
+                    if (vertexOf[theVertexNumber].pressure - e->weight < Pmin) {
+                        tag = true;
+                        break;
+                    }
+            }
         }
-        // 如果当前点压力不满足要求, 放置放大器, 更新当前点压力
-        if (vertexes[source].pressure < maxCost) {
-            vertexes[source].boosterHere = true;
-            vertexes[source].pressure = Pmax;
-            ++numberOfboosters;
+        if (tag == true || k == 1) {
+            boosterHere[level] = true;
+            ++number_of_boosters;
+            vertexOf[theVertexNumber].pressure = Pmax;
+            if (number_of_boosters >= numberOfBoosters)
+                return;
+            placeBooster(level + 1, number_of_boosters);
         }
-        // 更新当前点的degradeToEnd为最大消耗
-        vertexes[source].degradeToEnd = maxCost;
-        
+        else {
+            boosterHere[level] = false;
+            placeBooster(level + 1, number_of_boosters);
+        }
     }
 
     void solution1() {
