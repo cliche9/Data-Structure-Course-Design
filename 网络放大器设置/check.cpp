6b1aad544deaa6386d7f84d39e2631d13f947b3a
cliche <745356675@qq.com>
2021-05-15 15:37:42 +0800

    -

--- 网络放大器设置/check.cpp
+++ 网络放大器设置/check.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 using namespace std;
 
 int main() {
-    system("clang++ solution.cpp -o solution.cpp");
+    int start = 0, end = 0;
+
+    system("clang++ solution1.cpp -o solution1.cpp");
+    start = clock();
     for (int i = 1; i <= 100; i++) {
         cout << "正在测试数据" << i << endl;
         string name = to_string(i);
         string todo;
-        todo = "./solution.cpp < data/input/input" + name + ".in > data/output/" + name + ".out";
+        todo = "./solution1.cpp < data/input/input" + name + ".in > data/output/" + name + ".out";
         system(todo.c_str());
         todo = "diff data/output/" + name + ".out data/dagData/outputSTD/" + name + ".out";
         auto ret = system(todo.c_str());
@@ -19,6 +23,28 @@ int main() {
             puts("Accept");
         }
     }
-    puts("All Accept");
+    end = clock();
+    cout << "All Accept -- by backtrack -- time = " << double(end - start) / CLOCKS_PER_SEC << "s\n\n";
+
+    system("clang++ solution2.cpp -o solution2.cpp");
+    start = clock();
+    for (int i = 1; i <= 100; i++) {
+        cout << "正在测试数据" << i << endl;
+        string name = to_string(i);
+        string todo;
+        todo = "./solution2.cpp < data/input/input" + name + ".in > data/output/" + name + ".out";
+        system(todo.c_str());
+        todo = "diff data/output/" + name + ".out data/dagData/outputSTD/" + name + ".out";
+        auto ret = system(todo.c_str());
+        if (ret) {
+            puts("error");
+            return 0;
+        } else {
+            puts("Accept");
+        }
+    }
+    end = clock();
+    cout << "All Accept -- by branchjudge -- time = " << double(end - start) / CLOCKS_PER_SEC << "s\n\n";
+
     return 0;
 }
